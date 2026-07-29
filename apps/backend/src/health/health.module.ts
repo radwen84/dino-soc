0b6d767de60f5c6030bfd579d27f36d75eee3962
Radwen <radwen.cheichk@fsegt.utm.tn>
2026-07-29 16:38:02 +0200

    health1

--- apps/backend/src/health/health.module.ts
+++ apps/backend/src/health/health.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
+import { PrismaService } from '../prisma/prisma.service';
 import { RedisModule } from '../redis/redis.module';
 
 @Module({
   imports: [RedisModule],
   controllers: [HealthController],
+  providers: [PrismaService],
 })
 export class HealthModule {}
