60bc58f6c1c72477cc8202a4dc5f979379452bba
Radwen <radwen.cheichk@fsegt.utm.tn>
2026-09-08 19:15:24 +0200

    plawright1

--- apps/frontend/playwright.config.ts
+++ apps/frontend/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   // Pointer directement vers le dossier e2e dans apps/frontend
-  testDir: './apps/frontend/e2e',
+  testDir: './apps/backend/tests/e2e',
 
   // Cibler les fichiers de test .spec.ts ou .e2e.ts
   testMatch: '**/*.{spec,e2e}.{ts,js}',
